f9984f2cb58ce0e610fd12185caf0bde735339c0
JaeBin <dbwpqls9998@naver.com>
2022-07-13 16:42:16 +0900

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NaviBar from "./components/NaviBar";
 import Home from "./pages/Home";
-import Example from "./pages/Example";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 
@@ -15,7 +14,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/example" element={<Example />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
