58282926ac526bfd1174702c4975598fdc136014
JaeBin <dbwpqls9998@naver.com>
2022-07-22 10:50:22 +0900

    feat: 로고추가,이미지변경

--- frontend/src/components/NaviBar/index.tsx
+++ frontend/src/components/NaviBar/index.tsx
@@ -6,7 +6,7 @@ import "./style.css";
 const Navibar = () => {
     return (
             <div className="header">
-                <img className="logo" src="img/Logo.jpg" alt="이미지오류"></img>
+                <img className="logo" src="img/Logo.png" alt="이미지오류"></img>
                 <div className="header_nav">
                     <div className="fishlist">fishlist</div>
                     <div className="profile">profile</div>
