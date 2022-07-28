7b1c1bdaab352faaf3e84894962131aba3bf7724
Ryan <leehahnwoong@gmail.com>
2022-07-28 16:47:19 -0700

    [fix] cors headers

--- backend/teami/settings.py
+++ backend/teami/settings.py
@@ -53,18 +53,20 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",#리액트
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",#리액트
 
 ]
 CORS_ORIGIN_WHITELIST = [
     "https://localhost:3000",
     "https://127.0.0.1:8000",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'teami.urls'
 
