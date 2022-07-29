ab5be7b9413076be4a0db4b9f8bb3ef3fd6b90f4
JaeBin <dbwpqls9998@naver.com>
2022-07-29 14:11:28 +0900

    fix: 폰트파일수정

---
+++ frontend/src/components/infomationBlank/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./style.css";
+import InfoBoxInner from "../InfoBoxInner"
+function index() {
+  return (
+    <div className="box">
+      <div className="grayBox">
+        <img src="img/top.png" alt="이미지오류"></img>
+        <div className="whiteBox">
+          <div className='test'>
+            <div className="imgBox"></div>
+            <div className='commentBox'>
+              <span className="numbering"></span>
+              <span className="name"></span>
+              <div className="explain"></div>
+            </div>
+          </div>
+          <div className="infoBox">
+          <InfoBoxInner content_1='' content_2='' content_3='' title_1='' title_2='' title_3=''/>
+          </div>
+        </div>
+        <img src="img/under.png" alt="이미지오류"></img>
+      </div>
+      <div className='controller'>
+        <div><img src="img/crossButton.png" alt="이미지오류"></img></div>
+        <div><img src="img/button23.png" alt="이미지오류"></img></div>
+      </div>
+      <div><img src="img/Group 61.png" alt="이미지오류"></img></div>
+    </div>
+  )
+}
+
+export default index
