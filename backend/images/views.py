57a95669a546c9a0efddb331237a120bcfe506f2
김연진 <love_homebody@naver.com>
2022-08-02 21:48:28 +0900

    [feat] 토큰 만료 시간 수정

--- backend/images/views.py
+++ backend/images/views.py
@@ -71,7 +71,7 @@ class myFishList(APIView):
         image = images.get(id=image_id)
         serializer = imageSerializer(image)
         fish = Fish.objects.get(id=serializer.data.get('fish'))
-        content = {
+        result = {
             'url': serializer.data.get('url'), 
             'name': fish.name,
             'toxicity': fish.toxicity,
@@ -79,7 +79,7 @@ class myFishList(APIView):
             'prohibit_area': fish.prohibit_area,
             'description': fish.description
         }
-        return Response(content, status=status.HTTP_200_OK)
+        return Response(result, status=status.HTTP_200_OK)
 
     # 이미지 삭제
     @swagger_auto_schema(operation_id="특정 이미지 삭제")
